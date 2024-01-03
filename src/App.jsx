20ff46994594ff3aa8f50a6761ea4e4f7b312e34
EcaCosca <enriquecoscarelli@gmail.com>
2024-01-03 09:56:48 +0100

    LEt's practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,55 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState(null)
+
+  useEffect(()=>{
+    getData()
+  },[])
+
+  // EXAMPLE USING ASYNC AND AWAIT
+  // const getData = async () => {
+  //   const res = await fetch('https://fakestoreapi.com/products')
+  //   const data = await res.json()
+  //   console.log(data)
+  // }
+  
+  // EXAMPLE USING THEN
+  const getData = () => {
+    fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => setProducts(data))
+    .catch(err => console.log(err))
+  }
+
+  const getSingleProduct = (id) => {
+    console.log(id)
+    fetch(`https://fakestoreapi.com/products/${id}`)
+    .then(response => response.json())
+    .then(data => setProducts([data]))
+    .catch(err => console.log(err))
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Fake Store</h1>
+      <button onClick={()=>{getSingleProduct(3)}}>Get Jacket</button>
+      {
+        !products ? 
+        <h2>Loading...</h2>
+        :
+        products.map(product => (
+        <div key={product.id}>
+          <h2>{product.title}</h2>
+          <img src={product.image} alt={product.title} />
+          <p>{product.description}</p>
+        </div>
+      ))
+      }
     </>
   )
 }
